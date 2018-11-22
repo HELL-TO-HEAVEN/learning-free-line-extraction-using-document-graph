538280aa8229b5f881b8bca38f9f2af3a21e6124
Majeed Kassis <majeek@post.bgu.ac.il>
2018-11-22 19:17:46 +0200

    edge detection improvements
    added junction closing and junction thickining to improve graph edge detection

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import functools as ft
 import itertools as it
 import operator as op
 import numpy as np
+import time
 import cv2
 
 
@@ -75,6 +76,59 @@ def rotate(mat):
     return mat, mat.T, np.flipud(mat), np.fliplr(mat.T)
 
 
+# ---------------------------------------------------------------------------------
+# returns a median pixel in a collection of pixels
+# attempt to estimate the medial pixel as follows
+# for each pixel we find the pixels 'above' it and to the 'left' of it -> they increase a counter
+# for each same pixel we find the pixels 'below' it and to the 'right of it -> they decrease a counter
+# the closer the value to 0 the more 'centered' the pixel is
+# if more than one has same minimal value, we return the first one
+def median_pixel(pixels):
+    before_x = [sum(map(lambda p1: 1 if p1[0] > p2[0] else 0, pixels)) for p2 in pixels]
+    after_x = [sum(map(lambda p1: -1 if p1[0] < p2[0] else 0, pixels)) for p2 in pixels]
+    before_y = [sum(map(lambda p1: 1 if p1[1] > p2[1] else 0, pixels)) for p2 in pixels]
+    after_y = [sum(map(lambda p1: -1 if p1[1] < p2[1] else 0, pixels)) for p2 in pixels]
+
+    median_score = min(list(map(lambda x: abs(sum(x)), zip(before_x, after_x, before_y, after_y))))
+    return pixels[np.argmin(median_score)]
+
+
+# ---------------------------------------------------------------------------------
+# 'close' a junction to a square form. if the square surrounding the junction has missing pixels
+# we add them as well as long as they are part of the ridge matrix
+def close_junctions(junctions_mask, ridge_mask):
+    # cv2.imwrite("before.png", overlay_images(ridge_mask * 255, junctions_mask * 255))
+    candidates_list = set(map(tuple, np.argwhere(ridge_mask > 0)))
+    n_labels, labels = cv2.connectedComponents(junctions_mask, connectivity=8)
+    all_points = set()
+    for i in range(1, n_labels):
+        # for each label we retrieve its list of pixels
+        one_junction_pixels = set(map(tuple, np.argwhere(labels == i)))
+        # for junctions smaller than 5, we add a radius of 2 to the center pixel to increase the junction size
+        if len(one_junction_pixels) < 5:
+            median = median_pixel(list(one_junction_pixels))
+
+            one_junction_points = set(it.product(range(median[0] - 2, median[0] + 2),
+                                                 range(median[1] - 2, median[1] + 2)))
+        # otherwise, we 'close' the junction by adding to the junction non-marked pixels
+        # in the bounding square region of the junction pixels
+        else:
+            # find range = to make it square
+            x_values = list(map(lambda pixel: pixel[0], one_junction_pixels))
+            y_values = list(map(lambda pixel: pixel[1], one_junction_pixels))
+            one_junction_points = set(it.product(range(np.min(x_values), np.max(x_values) + 1),
+                                                 range(np.min(y_values), np.max(y_values) + 1)))
+        all_points = all_points.union(one_junction_points)
+
+    for point in set.intersection(set(all_points), candidates_list):
+        junctions_mask[point] = 1
+        candidates_list.remove(point)
+
+    # cv2.imwrite("after.png", overlay_images(ridge_mask*255, junctions_mask*255))
+    return junctions_mask
+
+# ---------------------------------------------------------------------------------
+# mark junction pixels using kernels and the ridge matrix
 def mark_junction_pixels(binary_image):
     junctions = map(uint8_array, [
         [
@@ -108,11 +162,11 @@ def mark_junction_pixels(binary_image):
 
     rotated_mats = (rotated for mat in junctions for rotated in rotate(mat))
 
-    junctions_mask = ft.reduce(op.or_, map(ft.partial(cv2.erode, binary_image), rotated_mats), np.zeros_like(binary_image))
-    # this is dilated to be used in edge extraction later
-    # dilation is used to increase the junction size (to cover the width of the skeleton)
-    # otherwise, we don't segment the graph to its edges properly once we remove the junctions
-    return cv2.dilate(junctions_mask, cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3, 3)))
+    junctions_mask = ft.reduce(op.or_, map(ft.partial(cv2.erode, binary_image), rotated_mats),
+                               np.zeros_like(binary_image))
+    # we need to close junctions -> find min_x min_y max_x max_y then add ridge pixels to it
+    # cv2.imwrite("before.png", overlay_images(binary_image*255, junctions_mask*255))
+    return close_junctions(junctions_mask, binary_image)
 
 
 # ---------------------------------------------------------------------------------
@@ -175,24 +229,40 @@ def get_vertexes(ridges_matrix, junction_pixels_mask):
     return vertexes_dictionary, vertexes_list, labels, vertexes_mask
 
 
+def save_image_like(image_like, pixel_list, name):
+    image = np.zeros_like(image_like)
+    for x in pixel_list:
+        image[x] = 255
+    res = overlay_images(image_like * 255, image)
+    cv2.imwrite(name + ".png", res)
+
+
+def view_image_like(image_like, pixel_list, name):
+    image = np.zeros_like(image_like)
+    for x in pixel_list:
+        image[x] = 255
+    res = overlay_images(image_like * 255, image)
+    cv2.imshow(name, res)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
 # ---------------------------------------------------------------------------------
 # edge extraction
 def get_edges(ridges_mask, junction_pixels_mask, vertexes_dictionary, vertexes_list):
-    vertexes_dictionary = set(list(it.chain.from_iterable(vertexes_dictionary.values())))
+    vertexes_set = set(list(it.chain.from_iterable(vertexes_dictionary.values())))
 
     # we remove the junctions from the ridge mask
     # this way we disconnect them from each other
     edges_mask = ridges_mask - junction_pixels_mask
     # each connected component is an edge in the graph
     n_edges, labels = cv2.connectedComponents(edges_mask, connectivity=8)
+    cv2.imwrite('0_edges_mask.png', edges_mask*255)
     # add to a dictionary - edge number, and its list of pixels
     edge_dictionary = {}
-    edges_list = []
     for i in range(1, n_edges):
         # for each label we retrieve its list of pixels
-        edge_pixels = np.argwhere(labels == i)
-        edge_pixels = list(map(tuple, edge_pixels))
-        print(len(edge_pixels))
+        edge_pixels = list(map(tuple, np.argwhere(labels == i)))
         edge_dictionary[i] = edge_pixels
 
         # iteratively - add in junction pixels that are nearby to the current edge
@@ -200,25 +270,34 @@ def get_edges(ridges_mask, junction_pixels_mask, vertexes_dictionary, vertexes_l
         # new pixels come only from junction pixels
         # this way the edge covers the vertexes as well
         not_empty = True
+        j = 0
+        save_image_like(ridges_mask, edge_pixels, str(i) + "_edge_mask_" + str(j))
         while not_empty:
             # add-in candidate junctions
             # 8-neighborhood of a pixel
             neighborhood = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (-1, -1), (-1, 1), (1, -1), (0, 0)]
             # calculate values of the pixels using the offsets for all edge_pixels of edge i
-            pixels_with_neighbors = list(tuple(map(op.add, pixel, offset))
-                                         for pixel in edge_pixels for offset in neighborhood)
+            pixels_with_neighbors = set(tuple(map(op.add, pixel, offset))
+                                        for pixel in edge_pixels for offset in neighborhood)
             # junction pixels are added back
-            candidate_edge_pixels = list(set.intersection(set(pixels_with_neighbors), vertexes_dictionary))
-            vertexes_dictionary = filter(lambda edge_pixel: edge_pixel in candidate_edge_pixels, vertexes_dictionary)
-            print(len(candidate_edge_pixels))
+            candidate_edge_pixels = list(set.intersection(pixels_with_neighbors, vertexes_set))
+            # print('candidate_edge_pixels=', len(candidate_edge_pixels))
+
+            # filter these pixels from junction list
+            vertexes_set = set(filter(lambda edge_pixel: edge_pixel not in candidate_edge_pixels, vertexes_set))
+            # save_image_like(ridges_mask, vertexes_set, str(i) + "_vertexes_set_after_" + str(j))
+            j += 1
+            # view_image_like(ridges_mask, vertexes_set, "vertexes_set_after")
+            # stopping criteria - once we can't add more pixels to the edge from junction pixels, we stop
+            # print(len(candidate_edge_pixels))
             if len(candidate_edge_pixels) == 0:
                 not_empty = False
             else:
-                print(edge_pixels)
-                print(candidate_edge_pixels)
                 edge_pixels += candidate_edge_pixels
-        # once this edge is complete
-        # TODO we need to find its vertexes
+            save_image_like(ridges_mask, edge_pixels, str(i) + "_edge_mask_" + str(j))
+        # once this edge is complete - we find its vertexes
+        res = list(set.intersection(set(edge_pixels), set(vertexes_list)))
+        print(res)
         # TODO then apply m-adjacency to find the shortest slimmest version of it
         one_edge_mask = np.zeros_like(ridges_mask)
         for x in edge_pixels:
@@ -274,14 +353,14 @@ def ridge_extraction(image_preprocessed):
 def pre_process(path):
     # load image as gray-scale, and convert to binary using otsu binarization
     image = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-    # TODO do we need binarization? better work on grayscale unless it does not work well
+    # TODO do we need binarization? better work on gray-scale unless it does not work well
     image = cv2.threshold(image, 0, 1, cv2.THRESH_OTSU)[1]
     # add white border around image of size 29
     white_border_added = cv2.copyMakeBorder(image, 29, 29, 29, 29, cv2.BORDER_CONSTANT, None, 1)
     # on top of that add black border of size 1
     black_border_added = cv2.copyMakeBorder(white_border_added, 1, 1, 1, 1, cv2.BORDER_CONSTANT, None, 0)
     # TODO erode the result to disconnect weakly connected components and removing white noise
-    # TODO return cv2.erode(black_border_added, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
+    # return cv2.erode(black_border_added, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3)))
     # TODO above code was removed - seems erode is not good(?) gotta check
     # return non-eroded image - maybe erosion is not good here
     return black_border_added
@@ -296,8 +375,10 @@ def run_all(path):
     ridges_mask, ridges_matrix = ridge_extraction(image_preprocessed)
     # mark junction pixels
     junction_pixels_mask = mark_junction_pixels(ridges_mask)
+    cv2.imwrite('junction_pixels_mask.png', overlay_images(ridges_mask*255, junction_pixels_mask*255))
     # retrieve vertex pixels
     vertexes_dictionary, vertexes_list, labels, vertex_mask = get_vertexes(ridges_matrix, junction_pixels_mask)
+    save_image_like(ridges_mask, vertexes_list, 'vertex_mask')
     # retrieve edges between two vertexes
     # each edge is a series of pixels from vertex u to vertex v
     edges_dictionary, edges_list = get_edges(ridges_mask, junction_pixels_mask, vertexes_dictionary, vertexes_list)
